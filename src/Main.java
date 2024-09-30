aa9e543a9124bb365478ca828145d8efae3afc16
Your Name <felipecamposc9@gmail.com>
2024-09-30 10:59:45 -0500

    DATOS PERSONALES

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+int n1=2;
+        int n2=2;
+        int suma =n1+n2;
+        System.out.println("la suma es "+suma);
+// NOMBRE : BRAYAN FELIPE CAMPOS CRISTANCHO
+//  CORREO :  BRAYAN.CAMPOS-C@UNIMINUTO.EDU.CO
+  //DIRECCION: TRANSVERSAL 5 A N 13 D 31
+  //HOBIE: HACER MUSICA CON INSTRUMENTOS DE PERCUSION
+
+    }
+}
